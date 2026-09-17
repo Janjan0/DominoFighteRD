475c3c41de8b0c3ad93e17bdb4476081519e4258
Janjan0 <104037652+Janjan0@users.noreply.github.com>
2026-09-17 17:57:30 -0400

    Updated index.ts

--- supabase/functions/domino-game/index.ts
+++ supabase/functions/domino-game/index.ts
@@ -6,9 +6,6 @@ const corsHeaders = {
   "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Client-Info, Apikey",
 };
 
-if (req.method === "OPTIONS") {
-  return new Response(null, { status: 200, headers: corsHeaders });
-}
 
 const supabaseUrl = Deno.env.get("SUPABASE_URL")!;
 const supabaseKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
